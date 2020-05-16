633e3ebdf2390f77bce46746745d9fd9c86e2f24
Freddy Brasileiro <freddybrasileiro@gmail.com>
2020-05-16 20:11:34 -0300

    :recycle: creating a component for NumberFormatCustom

---
+++ src/components/NumberFormatCustom/index.ts
@@ -0,0 +1,1 @@
+export { default as NumberFormatCustom } from './NumberFormatCustom';
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,41 +1,7 @@
 import React from 'react';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import MaterialTextField from '@material-ui/core/TextField';
-import NumberFormat from 'react-number-format';
-
-interface NumberFormatCustomProps {
-    inputRef: (instance: NumberFormat | null) => void;
-    onChange: (event: { target: { name: string; value: string } }) => void;
-    name: string;
-  }
-  
-  function NumberFormatCustom(props: NumberFormatCustomProps) {
-    const { inputRef, onChange, ...other } = props;
-  
-    return (
-      <NumberFormat
-        {...other}
-        getInputRef={inputRef}
-        onValueChange={(values) => {
-          onChange({
-            target: {
-              name: props.name,
-              value: values.value,
-            },
-          });
-        }}
-        thousandSeparator
-        isNumericString
-        allowNegative={false}
-        decimalScale={0}
-      />
-    );
-  }
-  
-  interface State {
-    textmask: string;
-    numberformat: string;
-  }
+import { NumberFormatCustom } from '../../components/NumberFormatCustom';
 
 type ButtonProps = {
     label: string,
@@ -43,25 +9,15 @@ type ButtonProps = {
     placeholder: string,
 };
 
-const TextField = ({ label, adornment, placeholder }: ButtonProps) => {
-    const adornmentProps = {
-        startAdornment: <InputAdornment position="start">{adornment}</InputAdornment>,
-    };
-    const inputProps = {
-        //inputComponent
-
-    };
-    if (!!adornment) inputProps
-    return (
-        <MaterialTextField
-            label={label}
-            placeholder={placeholder}
-            InputProps={{
-                startAdornment: <InputAdornment position="start">{adornment}</InputAdornment>,
-                inputComponent: NumberFormatCustom as any
-            }}
-        />
-    )
-};
+const TextField = ({ label, adornment, placeholder }: ButtonProps) => (
+    <MaterialTextField
+        label={label}
+        placeholder={placeholder}
+        InputProps={{
+            startAdornment: <InputAdornment position="start">{adornment}</InputAdornment>,
+            inputComponent: NumberFormatCustom as any
+        }}
+    />
+);
 
 export default TextField;
