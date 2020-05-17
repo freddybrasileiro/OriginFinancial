342ec6d2173c18a9757c80b666d2ccc502f0e14d
Freddy Brasileiro <freddybrasileiro@gmail.com>
2020-05-17 14:52:37 -0300

    :white_check_mark: writing tests for Simulation Result

---
+++ src/components/SimulationResult/SimulationResult.test.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { SimulationResult } from './index';
+
+describe('SimulationResult', () => {
+  it('Should render the SimulationResult component', () => {
+    expect(shallow(<SimulationResult totalAmount={25000} reachGoalBy={[4, 2024]} />)).toMatchSnapshot();
+  });
+});
--- src/components/SimulationResult/SimulationResult.tsx
+++ src/components/SimulationResult/SimulationResult.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 
 import {Card} from '../../components/Card';
 import {Text} from '../../components/Text';
