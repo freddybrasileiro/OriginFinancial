94489869ff6925419aacc71249765d13b4a0f002
Freddy Brasileiro <freddybrasileiro@gmail.com>
2020-05-17 22:30:08 -0300

    :green_heart: fixing CI build

--- spec/setup.js
+++ spec/setup.js
@@ -1,4 +1,4 @@
-const Adapter = await import('enzyme-adapter-react-16');
-const Enzyme = await import('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+const Enzyme = require('enzyme');
 
 Enzyme.configure({ adapter: new Adapter() });
